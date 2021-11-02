5fa9b7d9527bda4480bdd8690da393c547868bc4
Tiago Meireles <tiagocf475@gmail.com>
2021-11-02 15:43:02 -0300

    tenho q estilizar a parte de projetos

---
+++ Projeto-HTML/script.js
@@ -0,0 +1,19 @@
+document.querySelector('.list').addEventListener('click', () => {
+  window.open('./projetos/list');
+});
+
+document.querySelector('.pixel').addEventListener('click', () => {
+  window.open('./projetos/pixel');
+});
+
+document.querySelector('.memes').addEventListener('click', () => {
+  window.open('./projetos/meme');
+});
+
+document.querySelector('.color').addEventListener('click', () => {
+  window.open('./projetos/color');
+});
+
+document.querySelector('.carta').addEventListener('click', () => {
+  window.open('./projetos/carta');
+});
